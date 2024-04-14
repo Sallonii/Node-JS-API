bac21a7b03e47b74ad21a529f2b017e5ab06dba0
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-04-14 20:52:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
